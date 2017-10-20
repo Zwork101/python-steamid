5f5613f9baf2bcdb9c21665b58871fedd593373b
Nate the great <zwork101@gmail.com>
2017-10-20 16:16:00 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
 from setuptools import setup
 
 setup(
-    name = "steamid",
-    version = "1.0.0",
+    name="steamid",
+    version="1.0.0",
+    description="The python port of node-steamid (created by McKay)",
+    url="https://github.com/Zwork101/python-steamid",
+    author="Zwork101",
+    author_email="zwork101@gmail.com",
+    license='MIT',
 
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords="steam id"
 )
